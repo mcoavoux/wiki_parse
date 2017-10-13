0308e32c0b8124a451f8c6879c2c6be89ac0113d
maximinc <maximin.coavoux@gmail.com>
2017-10-13 12:51:05 +0200

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,29 @@
+
+
+## setup up necessary tools
+
+
+# Download wikiextractor
+git clone https://github.com/attardi/wikiextractor.git
+
+
+# Download and compile tokenizer for French:
+git clone https://github.com/mcoavoux/nlp-toolbox.git
+cd nlp-toolbox/tokenizer
+make clean
+make
+cp tokenizer ../../.
+cd ../..
+
+
+# Download and compile parser
+git clone https://github.com/mcoavoux/mtg.git
+cd mtg/mind_the_gap_v1.2/
+mkdir bin
+cd lib
+bash get_dependencies.bash
+cd ../src
+make clean
+make wstring
+cp mtg2_parser ../../../.
+cd ../../..
