0e990e824c19f274d10c918bdd2a3c8220704d20
maximinc <maximin.coavoux@gmail.com>
2017-10-13 13:23:17 +0200

    scripts for parsing wikiquote and wikipedia

--- parse_wiki_quote.sh
+++ parse_wiki_quote.sh
@@ -2,11 +2,11 @@
 wget https://dumps.wikimedia.org/other/cirrussearch/20171009/frwikiquote-20171009-cirrussearch-content.json.gz
 
 mkdir -p extracted_texts
-
 # make sure this calls python2
 python2 wikiextractor/cirrus-extract.py -o extracted_texts/frwikiquote frwikiquote-20171009-cirrussearch-content.json.gz
 
+model=mtg/mind_the_gap_v1.2/pretrained_models_projective/ml_lex/FRENCH/
 
-python3 parse_wiki.py mtg/mind_the_gap_v1.2/bin/mtg2_parser mtg/mind_the_gap_v1.2/pretrained_models_projective/ml_lex/FRENCH/ nlp-toolbox/tokenizer/ --threads 20
+python3 parse_wiki.py mtg2_parser ${model} nlp-toolbox/tokenizer/ --threads 20
 
 
