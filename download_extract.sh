a9f81ce6a54120bf9d2f59875e20d1e905b4576e
maximinc <maximin.coavoux@gmail.com>
2017-10-13 16:18:50 +0200

    update download script

--- download_extract.sh
+++ download_extract.sh
@@ -2,32 +2,17 @@
 
 prefix=https://dumps.wikimedia.org/other/cirrussearch/20171009/
 
+date=20171009
+suffix=-${date}-cirrussearch-content.json.gz
 
-wikibooks=frwikibooks-20171009-cirrussearch-content.json.gz
-wikinews=frwikinews-20171009-cirrussearch-content.json.gz
-wiki=frwiki-20171009-cirrussearch-content.json.gz
-wikiquote=frwikiquote-20171009-cirrussearch-content.json.gz
-wikiversity=frwikiversity-20171009-cirrussearch-content.json.gz
-wikivoyage=frwikivoyage-20171009-cirrussearch-content.json.gz
-wiktionary=frwiktionary-20171009-cirrussearch-content.json.gz
-
-
-wget ${prefix}/${wikibooks}
-wget ${prefix}/${wikinews}
-#wget ${prefix}/${wiki}
-wget ${prefix}/${wikiquote}
-wget ${prefix}/${wikisource}
-wget ${prefix}/${wikiversity}
-wget ${prefix}/${wikivoyage}
-wget ${prefix}/${wiktionary}
+#all=frwikibooks frwikinews frwiki frwikiquote frwikisource frwikiversity frwikivoyage frwiktionary
+all=frwikisource
 
 mkdir -p extracted_texts
+for wik in ${all}
+do
+    wget ${prefix}/${wik}-${suffix}
+    python2 wikiextractor/cirrus-extract.py -o extracted_texts/${wik} ${wik}-${suffix}
+done
 
-python2 wikiextractor/cirrus-extract.py -o extracted_texts/wikibooks ${wikibooks}
-python2 wikiextractor/cirrus-extract.py -o extracted_texts/wikinews ${wikinews}
-#python2 wikiextractor/cirrus-extract.py -o extracted_texts/wiki ${wiki}
-python2 wikiextractor/cirrus-extract.py -o extracted_texts/wikiquote ${wikiquote}
-python2 wikiextractor/cirrus-extract.py -o extracted_texts/wikisource ${wikisource}
-python2 wikiextractor/cirrus-extract.py -o extracted_texts/wikiversity ${wikiversity}
-python2 wikiextractor/cirrus-extract.py -o extracted_texts/wikivoyage ${wikivoyage}
-python2 wikiextractor/cirrus-extract.py -o extracted_texts/wiktionary ${wiktionary}
+
